55dc3ea3d8492b51e353c9483dcbb9d2636d6b5d
vibhav <vibhav.1507@gmail.com>
2022-05-03 10:50:13 +0530

    assignment 7

--- assignment 7/MCM.cpp
+++ assignment 7/MCM.cpp
@@ -1,41 +1,63 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
+#include<map>
+#include<queue>
+#include<stack>
+#include<set>
+#include<cmath>
+#include<string>
 #include<climits>
+#include<unordered_map>
+#include<unordered_set>
+#include<iomanip>
+#include<bitset>
 using namespace std;
+typedef long long ll;
 
-int MCM(int arr[], int n)
+int matrixChainMultiplication(vector<int>p)
 {
-    //matrix chain mnultiplication
-    int dp[100][100];
-    for (int i = 1; i < n; i++)
-        dp[i][i] = 0;
-    for (int gap = 2; gap < n; gap++)
+    int n=p.size();
+    vector<vector<int>>dp(n,vector<int>(n,0));
+     
+    //0th row and 0th colomn is always unused
+    //lower triangular matrix is 0
+    for(int i=1;i<n;i++)
     {
-        for (int i = 1; i < n - gap + 1; i++)
+        dp[i][i]=0;
+    }
+    for (int l = 2; l < n; l++)
+    {
+        for (int i = 1; i < n - l + 1; i++)
         {
-            int j = i + gap - 1;
+            int j = i + l - 1;
             dp[i][j] = INT_MAX;
-            for (int k = i; k <= j - 1; k++)
+            for (int k = i; k < j; k++)
             {
-
-                int q = dp[i][k] + dp[k + 1][j]
-                    + arr[i - 1] * arr[k] * arr[j];
+                int q = dp[i][k] + dp[k + 1][j] + p[i - 1] * p[k] * p[j];
                 if (q < dp[i][j])
                     dp[i][j] = q;
             }
         }
     }
- 
-    return dp[1][n - 1];
+    return dp[1][n-1];
 }
 
 int main()
 {
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
     int n;
     cin>>n;
-    int arr[100];
-    for (int i=0;i<n;i++)
+    vector<int>p(n);
+    for(int i=0;i<n;i++)
     {
-        cin>>arr[i];
+        cin>>p[i];
     }
-    cout<<"minimum number of multiplications"<<MCM(arr,n);
+    cout<<"Minimum number of multiplications is "<<matrixChainMultiplication(p);    
+
+    return 0;
 }
